462e1c086b6f2b01742a383c7f82d0c705835f20
wenruiLi <36104364+wenruiLiA15@users.noreply.github.com>
2022-10-26 18:08:49 +0800

    feat: test 5 times return Buzz

--- FizzBuzzTest/FizzBuzzTest.cs
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -15,10 +15,31 @@ namespace FizzBuzzTest
             int number = 1;
             var fizzBuzz = new FizzBuzz();
            //when
-           string shout = FizzBuzz.CountOff(number);
+            string shout = FizzBuzz.CountOff(number);
 
             //then
             Assert.Equal(number.ToString(), shout);
+        }
+
+        [Fact]
+        public void Should_return_Fizz_when_given_3times_number()
+        {
+            //given 
+            int number = 3;
+            var fizzBuzz = new FizzBuzz();
+            // when
+            string shout = FizzBuzz.CountOff(number);
+            // then
+            Assert.Equal("Fizz", shout);
+        }
+
+        [Fact]
+        public void Should_return_Buzz_when_given_5times_number()
+        {
+            int number = 5;
+            var fizzBuzz = new FizzBuzz();
+            var shout = FizzBuzz.CountOff(number);
+            Assert.Equal("Buzz",shout);
         }
     }
 }
