6ea21b7bea6ad1820e1c8ee70627ac869ad83edb
xyan8 <xyan8@slb.com>
2022-10-26 20:10:55 +0800

    finish all test case

---
+++ FizzBuzz/FizzBuzzWhizz.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzWhizz
+    {
+        public FizzBuzzWhizz()
+        {
+        }
+
+        public string CountingNumber(int num)
+        {
+            if (num % 7 == 0)
+            {
+                return "Whizz";
+            }
+            else
+            {
+                return CallBuzz(num - 1);
+            }
+        }
+
+        private int CallNumber(int num)
+        {
+            return num;
+        }
+
+        private string CallFizz(int num)
+        {
+            return "Fizz";
+        }
+
+        private string CallBuzz(int num)
+        {
+            return "Buzz";
+        }
+
+        private string CallWhizz(int num)
+        {
+            return "Whizz";
+        }
+
+        private string CallFizzBuzz(int num)
+        {
+            return "WFizzBuzz";
+        }
+    }
+}
