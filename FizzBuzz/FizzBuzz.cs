0caf5a05bd8d9c1499371e13655f468b4c8aab4a
Jiahui Xiong <jxiong5@slb.com>
2022-10-26 17:50:26 +0800

    feat: add multiple_5 case

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -4,8 +4,15 @@ namespace FizzBuzz
 {
     public class FizzBuzzGame
     {
+        private readonly int three = 3;
+        private readonly int five = 5;
         public string CountOff(int number)
         {
+            if (number % five == 0)
+            {
+                return "Buzz";
+            }
+
             return number.ToString();
         }
     }
--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace FizzBuzzTest
 {
-    public class Class1Test
+    public class FizzBuzzGameTest
     {
         [Fact]
         public void Should_normal_number_When_CountOff_Given_normal_number()
@@ -16,6 +16,18 @@ namespace FizzBuzzTest
             var result = fizzBuzz.CountOff(number);
             //then
             Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void Should_return_Buzz_When_CountOff_Given_multiple_of_5()
+        {
+            //given
+            var fizzBuzz = new FizzBuzzGame();
+            int number = 5;
+            //when
+            var result = fizzBuzz.CountOff(number);
+            //then
+            Assert.Equal("Buzz", result);
         }
     }
 }
